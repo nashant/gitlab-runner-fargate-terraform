c47b3c658432b6ccc1ada7cea4ad5280663d680b
Anthony Nash <anthony.nash@du.co>
2020-11-17 14:32:59 +0000

    Use SSH_PUBLIC_KEY provided by gitlab

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,16 +10,16 @@ propagateAWSEnvVarsAllLoginSessions() {
 }
 
 setUpSSH() {
-  if [ -z "$PUBLIC_KEY" ]; then
-    echo 'Need your SSH public key as the PUBLIC_KEY environment variable.'
+  if [ -z "$SSH_PUBLIC_KEY" ]; then
+    echo 'Need your SSH public key as the SSH_PUBLIC_KEY environment variable.'
     exit 1
   fi
   echo root:$(cat /dev/urandom | tr -dc _A-Z-a-z-0-9 | head -c${1:-16}) | chpasswd 2> /dev/null
   ssh-keygen -t rsa -b 2048 -f /etc/ssh/ssh_host_rsa_key -N ''
   USER_SSH_KEYS_FOLDER=~/.ssh
   [ -d "$USER_SSH_KEYS_FOLDER" ] || mkdir -p $USER_SSH_KEYS_FOLDER
-  echo $PUBLIC_KEY > ${USER_SSH_KEYS_FOLDER}/authorized_keys
-  unset PUBLIC_KEY
+  echo $SSH_PUBLIC_KEY > ${USER_SSH_KEYS_FOLDER}/authorized_keys
+  unset SSH_PUBLIC_KEY
   env | grep -E '^(AWS|ECS)_' >> ${USER_SSH_KEYS_FOLDER}/environment
   echo PATH="/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin" >> ${USER_SSH_KEYS_FOLDER}/environment
   /usr/sbin/sshd -Def /root/sshd_config
